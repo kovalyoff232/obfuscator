e40e050c74328e2a0f38d3ce7d25cbe186669a97
kovalyoff232 <metallbagaev5@gmail.com>
2025-07-25 23:47:16 +0300

    Refactor(expressions): Improve expression obfuscation and revert call indirection

--- pkg/obfuscator/expressions.go
+++ pkg/obfuscator/expressions.go
@@ -35,13 +35,15 @@ func ObfuscateExpressions(file *ast.File, info *types.Info) {
 		}
 
 		var newExpr ast.Expr
+		template := randInt(2)
+
 		switch binaryExpr.Op {
 		case token.ADD:
-			newExpr = obfuscateAdd(binaryExpr.X, binaryExpr.Y)
+			newExpr = obfuscateAdd(binaryExpr.X, binaryExpr.Y, int(template))
 		case token.SUB:
-			newExpr = obfuscateSub(binaryExpr.X, binaryExpr.Y)
+			newExpr = obfuscateSub(binaryExpr.X, binaryExpr.Y, int(template))
 		case token.XOR:
-			newExpr = obfuscateXor(binaryExpr.X, binaryExpr.Y)
+			newExpr = obfuscateXor(binaryExpr.X, binaryExpr.Y, int(template))
 		}
 
 		if newExpr != nil {
@@ -53,45 +55,71 @@ func ObfuscateExpressions(file *ast.File, info *types.Info) {
 	}, nil)
 }
 
-// a + b -> (a ^ b) + 2 * (a & b)
-func obfuscateAdd(x, y ast.Expr) ast.Expr {
-	return &ast.BinaryExpr{
-		X: &ast.ParenExpr{
-			X: &ast.BinaryExpr{X: x, Op: token.XOR, Y: y},
-		},
-		Op: token.ADD,
-		Y: &ast.BinaryExpr{
-			X:  &ast.BasicLit{Kind: token.INT, Value: "2"},
-			Op: token.MUL,
-			Y: &ast.ParenExpr{
-				X: &ast.BinaryExpr{X: x, Op: token.AND, Y: y},
-			},
-		},
-	}
-}
-
-// a - b -> a + ^b + 1
-func obfuscateSub(x, y ast.Expr) ast.Expr {
-	return &ast.BinaryExpr{
-		X: &ast.BinaryExpr{
-			X:  x,
+func obfuscateAdd(x, y ast.Expr, template int) ast.Expr {
+	switch template {
+	case 0:
+		// a + b -> (a ^ b) + 2 * (a & b)
+		return &ast.BinaryExpr{
+			X: &ast.ParenExpr{X: &ast.BinaryExpr{X: x, Op: token.XOR, Y: y}},
 			Op: token.ADD,
-			Y:  &ast.UnaryExpr{Op: token.XOR, X: y},
-		},
-		Op: token.ADD,
-		Y:  &ast.BasicLit{Kind: token.INT, Value: "1"},
+			Y: &ast.BinaryExpr{
+				X:  &ast.BasicLit{Kind: token.INT, Value: "2"},
+				Op: token.MUL,
+				Y:  &ast.ParenExpr{X: &ast.BinaryExpr{X: x, Op: token.AND, Y: y}},
+			},
+		}
+	default:
+		// a + b -> (a | b) + (a & b)
+		return &ast.BinaryExpr{
+			X: &ast.ParenExpr{X: &ast.BinaryExpr{X: x, Op: token.OR, Y: y}},
+			Op: token.ADD,
+			Y: &ast.ParenExpr{X: &ast.BinaryExpr{X: x, Op: token.AND, Y: y}},
+		}
 	}
 }
 
-// a ^ b -> (a | b) &^ (a & b)
-func obfuscateXor(x, y ast.Expr) ast.Expr {
-	return &ast.BinaryExpr{
-		X: &ast.ParenExpr{
-			X: &ast.BinaryExpr{X: x, Op: token.OR, Y: y},
-		},
-		Op: token.AND_NOT, // Bit clear (AND NOT)
-		Y: &ast.ParenExpr{
-			X: &ast.BinaryExpr{X: x, Op: token.AND, Y: y},
-		},
+func obfuscateSub(x, y ast.Expr, template int) ast.Expr {
+	switch template {
+	case 0:
+		// a - b -> a + ^b + 1
+		return &ast.BinaryExpr{
+			X:  &ast.BinaryExpr{X: x, Op: token.ADD, Y: &ast.UnaryExpr{Op: token.XOR, X: y}},
+			Op: token.ADD,
+			Y:  &ast.BasicLit{Kind: token.INT, Value: "1"},
+		}
+	default:
+		// a - b -> (a^b) - 2*(!a & b)
+		return &ast.BinaryExpr{
+			X: &ast.ParenExpr{X: &ast.BinaryExpr{X: x, Op: token.XOR, Y: y}},
+			Op: token.SUB,
+			Y: &ast.BinaryExpr{
+				X:  &ast.BasicLit{Kind: token.INT, Value: "2"},
+				Op: token.MUL,
+				Y: &ast.ParenExpr{X: &ast.BinaryExpr{
+					X:  &ast.UnaryExpr{Op: token.XOR, X: x},
+					Op: token.AND,
+					Y:  y,
+				}},
+			},
+		}
+	}
+}
+
+func obfuscateXor(x, y ast.Expr, template int) ast.Expr {
+	switch template {
+	case 0:
+		// a ^ b -> (a | b) &^ (a & b)
+		return &ast.BinaryExpr{
+			X:  &ast.ParenExpr{X: &ast.BinaryExpr{X: x, Op: token.OR, Y: y}},
+			Op: token.AND_NOT,
+			Y:  &ast.ParenExpr{X: &ast.BinaryExpr{X: x, Op: token.AND, Y: y}},
+		}
+	default:
+		// a ^ b -> (a &^ b) | (b &^ a)
+		return &ast.BinaryExpr{
+			X:  &ast.ParenExpr{X: &ast.BinaryExpr{X: x, Op: token.AND_NOT, Y: y}},
+			Op: token.OR,
+			Y:  &ast.ParenExpr{X: &ast.BinaryExpr{X: y, Op: token.AND_NOT, Y: x}},
+		}
 	}
 }
